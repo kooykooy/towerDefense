382bef6f36427b38550611e9801fe506d691cf67
koy <kevinh.dev@gmail.com>
2018-07-14 18:04:55 +0200

    add zombiedata + movementspeed

--- Assets/Scripts/zombieController.cs
+++ Assets/Scripts/zombieController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour {
 
+    ZombieData zombieData;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        zombieData = ScriptableObject.CreateInstance<ZombieData>();        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.right * Time.deltaTime);
+        transform.Translate(Vector2.right * Time.deltaTime *  zombieData.movementSpeed);
 	}
 }
